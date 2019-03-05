ed3ce1c628c9e0e3c525729a0854fb31e7f64ef2
Frank Hellmann <hellmann@pik-potsdam.de>
2019-03-05 17:32:04 +0100

    Tests added

--- testing_exp.jl
+++ testing_exp.jl
@@ -1,3 +1,36 @@
+using LightGraphs
+
+function test_diffusive()
+    G =barabasi_albert(30, 4)
+
+    edges! = [NetworkDynamics_exp.diffusion_edge! for e in edges(G)]
+    vertices! = [NetworkDynamics_exp.diffusion_vertex! for e in vertices(G)]
+
+    dim_v = ones(Int32, nv(G))
+    dim_e = ones(Int32, ne(G))
+
+    L = laplacian_matrix(G)
+
+    rhs = NetworkDynamics_exp.multi_static(vertices!, edges!, G, dim_v, dim_e)
+    rhs2 = (dx, x, p, t) -> dx .= - L * x
+
+    for _ in 1:10
+
+        x0 = rand(sum(dim_v))
+        dx0 = rand(sum(dim_v))
+        dx1 = rand(sum(dim_v))
+
+        p = nothing
+        t = 0.
+
+        rhs(dx0, x0, p, t)
+        rhs2(dx1, x0, p, t)
+
+        @assert isapprox(dx0, dx1)
+    end
+    true
+end
+
 begin
     include("src/NetworkDynamics_exp.jl")
 
