3004d769a8beeb0d8662b0a06d6ed4f6226f0aa4
Frank Hellmann <hellmann@pik-potsdam.de>
2019-03-21 14:57:44 +0100

    Restructuring for V0.1

--- src/Constructors.jl
+++ src/Constructors.jl
@@ -1,11 +1,31 @@
 using LightGraphs
 using DifferentialEquations
 
-function network_dynamics(Array{VertexFunction}, Array{EdgeFunction}, graph)
+#=
+The internal constructors are named as Vertex type, edge type and scalarity:
+
+_network_dynamics_ODE_ODE
+_network_dynamics_ODE_Static
+_network_dynamics_ODE_ODE_scalar
+_network_dynamics_ODE_Static_scalar
+=#
+
+function network_dynamics(vertices!,  edges!, graph)
+    try
+        va = Array{VertexFunction}(vertices!)
+    catch err
+        println("Cannot convert the Vertices to an Array{VertexFunction}!")
+        println(err)
+        return nothing
+    end
+
+    ve = Array{EdgeFunction}(edges!)
+    # Here goes the logic that tries to create a homogeneous array of Vertex and
+    # Edge types...
     nothing
 end
 
-function network_dynamics(Array{ODEVertex}, Array{StaticEdge}, graph)
+function network_dynamics(vertices!::Array{ODEVertex}, edges!::Array{StaticEdge}, graph)
     nothing
 end
 
@@ -21,9 +41,9 @@ function network_dynamics(vertices!::Array{ODEVertex}, edges!::Array{ODEEdge}, g
     massmatrix = sparse(1.0I,dim_nd,dim_nd) # Construct Mass Matrix from vertices! and edges!
 
     if all(dim_v .== 1) && all(dim_e .== 1)
-        nd! = scalar_network_dynamics(vertex_functions, ...)
+        nd! = _network_dynamics_ODE_ODE_scalar(vertex_functions, ...)
     else
-        nd! = full_network_dynamics(vertex_functions, dim_v, ...)
+        nd! = _network_dynamics_ODE_ODE(vertex_functions, dim_v, ...)
     end
 
     for i, idx in enumerate(nd!.e_idx)
@@ -32,7 +52,7 @@ function network_dynamics(vertices!::Array{ODEVertex}, edges!::Array{ODEEdge}, g
     for i, idx in enumerate(nd!.e_idx)
         massmatrix[idx,idx] = edges![i].massmatrix
     end
-
+    # Test here if massmatrix is proporitonal to the identity and if so drop it.
     ODEFunction(nd!, massmatrix)
 end
 
