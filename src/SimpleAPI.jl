4a6e458c0e89633aef6b84315ef60e1a77f10ddf
Frank Hellmann <hellmann@pik-potsdam.de>
2019-11-25 19:11:46 +0100

    Rewrite complete, with tests passing. The API for network dynamics has changed with this. You now need to pass a vector of parameters as well. This can be reverted, as the JacVecOperator stuff has been removed for now. It simply causes too many hickups with the solvers we need at the moment.
    test_rewrite_kura.jl contains an example that shows that something isn't 
    quite working yet though. We need to figure out what that is.

--- src/SimpleAPI.jl
+++ src/SimpleAPI.jl
@@ -3,24 +3,30 @@ export homogeneous_scalar_network_with_sum
 # """
 # Implement dx_i/dt = f(x_i, p, t) + \sum_j A_{ij} h(x_i, x_j, p, t) with scalar x
 # """
-function homogeneous_scalar_network_with_sum(f, h, graph)
 
-    function e!(e, x_s, x_t, p, t)
+"""
+This function builds the homogeneous network of the form:
+
+`dx_i/dt = f(x_i, p, t) + \\sum_j A_{ij} h(x_i, x_j, p, t)` with scalar variable `x`
+"""
+function homogeneous_scalar_network_with_sum(f, h, graph, p)
+
+    @inline function hsws_e!(e, x_s, x_t, p, t)
         e[1] = h(x_s[1], x_t[1], p, t)
         e[2] = h(x_t[1], x_s[1], p, t)
         nothing
     end
 
-    function v!(dv, v, e_s, e_t, p, t)
+    @inline function hsws_v!(dv, v, e_s, e_t, p, t)
         dv[1] = f(v[1], p, t) + sum( [e[1] for e in e_s] ) + sum( [e[2] for e in e_t] )
         nothing
     end
 
-    odevertex = ODEVertex(f! = v!, dim = 1)
-    staticedge = StaticEdge(f! = e!, dim = 2)
+    odevertex = ODEVertex(f! = hsws_v!, dim = 1)
+    staticedge = StaticEdge(f! = hsws_e!, dim = 2)
 
     vertex_list = [odevertex for v in vertices(graph)]
     edge_list = [staticedge for e in edges(graph)]
 
-    network_dynamics(vertex_list, edge_list, graph)
+    network_dynamics(vertex_list, edge_list, graph, p)
 end
--- test_rewrite_kura.jl
+++ test_rewrite_kura.jl
@@ -4,7 +4,7 @@ using OrdinaryDiffEq
 using DiffEqOperators
 using BenchmarkTools
 
-N = 10^3
+N = 10
 g = barabasi_albert(N,5)
 
 B = incidence_matrix(g, oriented=true)
@@ -24,13 +24,18 @@ end
 ω = randn(nv(g))
 
 kn = kuramoto_dyn(B, transpose(B), ω, N)
-Jv = JacVecOperator(kn, randn(nv(g)), nothing, 0.0)
-kur_network_L = ODEFunction(kn, jac_prototype=Jv)
+# Jv = JacVecOperator(kn, randn(nv(g)), nothing, 0.0)
+kur_network_L = ODEFunction(kn) #, jac_prototype=Jv)
 
 #Now for NetworkDynamics
 
 @inline Base.@propagate_inbounds function kuramoto_edge!(e,v_s,v_d,p,t)
     e[1] = sin(v_s[2] - v_d[2])
+    nothing
+end
+
+@inline Base.@propagate_inbounds function kuramoto_dedge!(de, e,v_s,v_d,p,t)
+    de[1] = 100. * (sin(v_s[2] - v_d[2]) - e[1])
     nothing
 end
 
@@ -49,26 +54,50 @@ end
 end
 
 
-odevertex = ODEVertex(f! = kuramoto_vertex!,dim = 2)
+odevertex = ODEVertex(f! = kuramoto_vertex!, dim = 2, sym=[:ω, :ϕ])
 staticedge = StaticEdge(f! = kuramoto_edge!, dim = 1)
 
 vertex_list = [odevertex for v in vertices(g)]
 edge_list = [staticedge for e in edges(g)]
+ode_sd_edge_list = [ODEEdge(se) for se in edge_list]
+ode_edge_list = [ODEEdge(f! = kuramoto_dedge!, dim = 1) for e in edges(g)]
 p = vcat(ω, zeros(ne(g)))
 
 kur_network_nd = network_dynamics(vertex_list,edge_list,g, p)
+kur_network_eode = network_dynamics(vertex_list,ode_edge_list,g, p)
 
 
 x0 = rand(2N)
 dx_L = similar(x0)
 dx_nd = similar(x0)
 
+x_ode = rand(2N+ne(g))
+dx_ode = similar(x_ode)
+
+println("A new way to interact with the state of the system is to call the
+network rhs with the signature (x, p, t, GetGD). This will return a GraphData
+object built on x, that allows reading and manipulating the undelrying data.")
+
+println("gd_x0 = kur_network_nd(x0, p, 0., GetGD)")
+gd_x0 = kur_network_nd(x0, p, 0., GetGD) # This provides us with the graph view of the x0 data
+gd_x0.e[1]
+println("x0[1] == $(x0[1])")
+println("gd_x0.v[1][1] == $(gd_x0.v[1][1])")
+gd_x0.v[1][1] = 5.
+println("x0[1] == $(x0[1])")
+println("gd_x0.v[1][1] == $(gd_x0.v[1][1])")
+
+
 kur_network_nd(dx_nd, x0, p, 0.)
+kur_network_eode(dx_ode, x_ode, p, 0.)
 kur_network_L(dx_L, x0, nothing, 0.)
 
-dx_nd[1:2:2N] .- dx_L[1:N] .|> abs |> maximum
+ω_idx = idx_containing(kur_network_nd, :ω)
+
+dx_nd[ω_idx] .- dx_L[1:N] .|> abs |> maximum
 
 println("Benchmarking")
 
 @btime kur_network_nd(dx_nd, x0, p, 0.)
 @btime kur_network_L(dx_L, x0, nothing, 0.)
+@btime kur_network_eode(dx_ode, x_ode, p, 0.)
